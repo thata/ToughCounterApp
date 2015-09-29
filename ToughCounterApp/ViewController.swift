4d87997d7565994a502131a0b85a77f52d4683bb
takashi hatakeyama <takashi.hatakeyama@gmail.com>
2015-09-29 18:51:05 +0900

    init

--- ToughCounterApp/ViewController.swift
+++ ToughCounterApp/ViewController.swift
@@ -10,16 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webView: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        loadWebPage()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    func loadWebPage() {
+        let url:NSURL? = NSURL(string: "https://tough-counter.herokuapp.com/")
+        let request:NSURLRequest = NSURLRequest(URL: url!)
+        if let _webView = self.webView {
+            _webView.loadRequest(request)
+        }
+    }
 }
 
